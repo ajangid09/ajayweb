86587e20ef1f69437b0593bfdf220b2b67071524
ajangid09 <117667939+ajangid09@users.noreply.github.com>
2022-11-07 21:43:20 +0530

    Add files via upload

---
+++ demo/js/slider.js
@@ -0,0 +1,39 @@
+$(function () {
+    $('.owl-carousel2').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+
+})
+
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+});
+
